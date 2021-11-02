acbf7774631061eb979e6bec1a1b976d49dcd520
ink2006 <gangun.park94@gmail.com>
2021-11-02 12:38:54 +0900

    submachinegun 아이템 추가 인벤토리 우클릭시 자동 교체

--- FPS_Defense/Assets/Scripts/UI/Slot.cs
+++ FPS_Defense/Assets/Scripts/UI/Slot.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerClickHandler
 {
 
     public Item item; // 획득한 아이템.
@@ -16,6 +17,13 @@ public class Slot : MonoBehaviour
     private Text text_Count;
     [SerializeField]
     private GameObject go_CountImage;
+
+    private WeaponManager theWeaponManager;
+
+    void Start()
+    {
+        theWeaponManager = FindObjectOfType<WeaponManager>();
+    }
 
 
     // 이미지의 투명도 조절.
@@ -67,5 +75,27 @@ public class Slot : MonoBehaviour
 
         text_Count.text = "0";
         go_CountImage.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            if(item != null)
+            {
+                if(item.itemType == Item.ItemType.Equipment) // 아이템이 장비아이템일 경우
+                {
+                    //장착
+                    StartCoroutine(theWeaponManager.ChangeWeaponCoroutine(item.weaponType, item.itemName));
+                    
+                }
+                else
+                {
+                    //소모
+                    Debug.Log(item.itemName + "을 사용했습니다.");
+                    SetSlotCount(-1);
+                }
+            }
+        }
     }
 }
