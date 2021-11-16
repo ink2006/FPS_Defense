775dd903d984dd7dec3cc3a72d077268c66f9c37
ink2006 <gangun.park94@gmail.com>
2021-11-16 14:59:20 +0900

    낮밤 구현 Light, Fog

---
+++ FPS_Defense/Assets/Scripts/DayAndNight.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour
+{
+    [SerializeField] private float secondPerRealTimeSecound; // 게임시간
+
+    private bool isNight = false;
+
+    [SerializeField] private float fogDensityCalc; // fog증가율
+
+    [SerializeField] private float nightFogDensity; // 밤 fog
+    private float dayFogDensity;
+    private float currentFogDensity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dayFogDensity = RenderSettings.fogDensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right, 0.1f * secondPerRealTimeSecound * Time.deltaTime);
+
+        //밤이 되면
+        if (transform.eulerAngles.x >= 170)
+            isNight = true;
+        else if (transform.eulerAngles.x <= 10)
+            isNight = false;
+
+        if(isNight)
+        {
+            if(currentFogDensity <= nightFogDensity)
+            {
+                currentFogDensity += 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;
+            }
+        }
+
+        else
+        {
+            if (currentFogDensity <= nightFogDensity)
+            {
+                currentFogDensity -= 0.1f * fogDensityCalc * Time.deltaTime;
+                RenderSettings.fogDensity = currentFogDensity;
+            }
+        }
+
+    }
+}
